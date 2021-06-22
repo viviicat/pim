9c2eae7a8d2afb05dbbdcf4a358d9b4d5423d0ea
Vivianne Langdon <puttabutta@gmail.com>
2021-06-22 04:34:59 -0700

    Pretty darn efficient, it seems

--- script/tests/vector_test.lua
+++ script/tests/vector_test.lua
@@ -4,14 +4,18 @@ local vector_test = {}
 
 function vector_test:update()
   local total = 0
+  local totalLen = 0
 
   for i=1, 10000 do
     local sun_vector = Cvar.get("r_sun_col")
     total = total + sun_vector.x
     total = total + sun_vector.y
     total = total + sun_vector.z
+    
+    totalLen = totalLen + sun_vector.length3
   end
   
+  Log.info(total, " ", totalLen)
 end
 
 Game.start_update(vector_test);
--- src/scriptsys/scr_cvar.c
+++ src/scriptsys/scr_cvar.c
@@ -15,65 +15,110 @@ pim_inline static float4 VEC_CALL scr_tof4(lua_State* L)
 	return *(float4*)luaL_checkudata(L, 1, SCR_FLOAT4_MT);
 }
 
-static i32 scr_f4_x(lua_State* L)
-{
-	float4 vec = scr_tof4(L);
-	lua_pushnumber(L, vec.x);
-	return 1;
-}
-
-static i32 scr_f4_y(lua_State* L)
-{
-	float4 vec = scr_tof4(L);
-	lua_pushnumber(L, vec.y);
-	return 1;
-}
-
-static i32 scr_f4_z(lua_State* L)
-{
-	float4 vec = scr_tof4(L);
-	lua_pushnumber(L, vec.z);
-	return 1;
-}
-
-static i32 scr_f4_w(lua_State* L)
-{
-	float4 vec = scr_tof4(L);
-	lua_pushnumber(L, vec.w);
-	return 1;
-}
-
-static i32 scr_f4_tostring(lua_State* L)
+pim_inline static i32 scr_func_tostring(lua_State* L)
 {
 	float4 vec = scr_tof4(L);
 	lua_pushfstring(L, "<%f, %f, %f, %f>", vec.x, vec.y, vec.z, vec.w);
 	return 1;
 }
 
-static i32 scr_f4_getters(lua_State* L)
+pim_inline static i32 VEC_CALL scr_func_x(lua_State* L, float4 vec)
 {
-	float4 vec = scr_tof4(L);
-	const char* getter = luaL_checkstring(L, 2);
-	if (!getter || getter[1]) // if 0 or longer than 1
-	{
-		return 0;
-	}
-
-	switch (getter[0])
-	{
-	case 'x': lua_pushnumber(L, vec.x); break;
-	case 'y': lua_pushnumber(L, vec.y); break;
-	case 'z': lua_pushnumber(L, vec.z); break;
-	case 'w': lua_pushnumber(L, vec.w); break;
-	default: return 0;
-	}
-
+	lua_pushnumber(L, vec.x);
 	return 1;
 }
 
+pim_inline static i32 VEC_CALL scr_func_y(lua_State* L, float4 vec)
+{
+	lua_pushnumber(L, vec.y);
+	return 1;
+}
+
+pim_inline static i32 VEC_CALL scr_func_z(lua_State* L, float4 vec)
+{
+	lua_pushnumber(L, vec.z);
+	return 1;
+}
+
+pim_inline static i32 VEC_CALL scr_func_w(lua_State* L, float4 vec)
+{
+	lua_pushnumber(L, vec.w);
+	return 1;
+}
+
+pim_inline static i32 VEC_CALL scr_func_sum(lua_State* L, float4 vec)
+{
+	lua_pushnumber(L, f4_sum(vec));
+	return 1;
+}
+
+pim_inline static i32 VEC_CALL scr_func_sum3(lua_State* L, float4 vec)
+{
+	lua_pushnumber(L, f4_sum3(vec));
+	return 1;
+}
+
+pim_inline static i32 VEC_CALL scr_func_length4(lua_State* L, float4 vec)
+{
+	lua_pushnumber(L, f4_length4(vec));
+	return 1;
+}
+
+pim_inline static i32 VEC_CALL scr_func_length3(lua_State* L, float4 vec)
+{
+	lua_pushnumber(L, f4_length3(vec));
+	return 1;
+}
+
+pim_inline static i32 VEC_CALL scr_func_lengthsq4(lua_State* L, float4 vec)
+{
+	lua_pushnumber(L, f4_lengthsq4(vec));
+	return 1;
+}
+
+pim_inline static i32 VEC_CALL scr_func_lengthsq3(lua_State* L, float4 vec)
+{
+	lua_pushnumber(L, f4_lengthsq3(vec));
+	return 1;
+}
+
+typedef struct scrGetter_f4 {
+	const char* name;
+	i32 (VEC_CALL *func) (lua_State* L, float4 vec);
+} scrGetter_f4;
+
+static const scrGetter_f4 sm_f4_funcs[] =
+{
+	LUA_FN(x),
+	LUA_FN(y),
+	LUA_FN(z),
+	LUA_FN(w),
+	LUA_FN(sum),
+	LUA_FN(sum3),
+	LUA_FN(length4),
+	LUA_FN(length3),
+	LUA_FN(lengthsq4),
+	LUA_FN(lengthsq3),
+};
+
+static i32 scr_func_getters(lua_State* L)
+{
+	for (i32 i = 1; i <= NELEM(sm_f4_funcs); i++)
+	{
+		if (lua_rawequal(L, 2, lua_upvalueindex(i)))
+		{
+			float4 vec = scr_tof4(L);
+			sm_f4_funcs[i - 1].func(L, vec);
+			return 1;
+		}
+	}
+
+	return 0;
+}
+
 static const luaL_Reg sm_f4_metameth[] = {
-	{ "__index", scr_f4_getters }, // placeholder
-	{ "__tostring", scr_f4_tostring },
+	{ "__index", scr_func_getters }, // placeholder
+	{ "__tostring", scr_func_tostring },
 	{ 0 }
 };
 
@@ -81,7 +126,18 @@ static const luaL_Reg sm_f4_metameth[] = {
 static void scr_f4_createmeta(lua_State* L)
 {
 	luaL_newmetatable(L, SCR_FLOAT4_MT);
-	luaL_setfuncs(L, sm_f4_metameth, 0);
+
+	// Here we are creating upvalues (closure variables)
+	// which we will apply to the __index metamethod. These
+	// upvalues are Lua strings representing the func names,
+	// so we can quickly compare the field names without having
+	// to do strcmp
+	for (i32 i = 0; i < NELEM(sm_f4_funcs); i++)
+	{
+		lua_pushstring(L, sm_f4_funcs[i].name);
+	}
+
+	luaL_setfuncs(L, sm_f4_metameth, NELEM(sm_f4_funcs));
 	lua_pop(L, 1);
 }
 
